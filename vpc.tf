75e8d58e19666c4c86a4d425592561bf355f18d7
CaseyJohnsonOH <cjjohnson@gmail.com>
2020-01-07 19:57:05 +0000

    Final battle

--- vpc.tf
+++ vpc.tf
@@ -16,3 +16,12 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+resource "aws_vpc" "gaurav" {
+  cidr_block       = "172.16.0.0/24"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "Gaurav"
+  }
+}
